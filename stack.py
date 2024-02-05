5bdb5d8dcefc55f648640d4a9bbe6d9d4002f59d
ndambuki8 <ndambukikevo8@gmail.com>
2024-02-05 05:03:18 +0300

    stack data structure

---
+++ stack.py
@@ -0,0 +1,31 @@
+#Stack implementation on python
+
+#creating a stack
+def create_stack():
+    stack = []
+    return stack
+
+#create an empty stack
+def check_empty(stack):
+    return len(stack) == 0
+
+#Adding items into the stack
+def push(stack, item):
+    stack.append(item)
+    print("Pushed item: " + item)
+
+
+#Removing an item from the stack
+def pop(stack):
+    if (check_empty(stack)):
+        return "stack is empty"
+    
+    return stack.pop()
+
+stack = create_stack()
+push(stack, str(1))
+push(stack, str(2))
+push(stack, str(3))
+push(stack, str(4))
+print("popped item: " + pop(stack))
+print("Stack after popping an element: " + str(stack))
