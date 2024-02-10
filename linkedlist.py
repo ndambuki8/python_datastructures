1e423d750bcf67ebaf78074c38d29ff3c003c6b0
ndambuki8 <ndambukikevo8@gmail.com>
2024-02-10 11:53:12 +0300

    linked list printing node content

--- linkedlist.py
+++ linkedlist.py
@@ -9,3 +9,20 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
+
+    #Assign item values
+    linked_list.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    #connect nodes
+    linked_list.head.next = second
+    second.next = third
+
+    #print the linked list item
+    while linked_list.head != None:
+        print(linked_list.head.item, end="")
+        linked_list.head = linked_list.head.next
